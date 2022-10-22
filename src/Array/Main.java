58c585b1ef546e12795ba7201d5fc4a61348fb87
raul_ <raul_nunes07@hotmail.com>
2022-10-22 20:17:25 -0300

    testando git

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -8,6 +8,9 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
 
+
+
+
         Scanner sc = new Scanner(System.in);
 
         double largura = sc.nextDouble();
